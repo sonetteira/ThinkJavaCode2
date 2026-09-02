d07c1654e5f53f3b724a280e9f0dca44969249e6
Leanne <lkeeley@pace.edu>
2026-09-02 10:38:00 -0400

    sample commit

--- appa/SeriesTest.java
+++ appa/SeriesTest.java
@@ -6,6 +6,7 @@ import junit.framework.TestCase;
 public class SeriesTest extends TestCase {
 
     public void testFibonacci() {
+        // this tests the fibonacci method
         assertEquals(1, Series.fibonacci(1));
         assertEquals(1, Series.fibonacci(2));
         assertEquals(2, Series.fibonacci(3));
